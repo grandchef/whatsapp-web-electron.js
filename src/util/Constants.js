fb4bc09cc8ae20cea1bde22aa11fdf5060926a95
samuelcust <samuelcust@users.noreply.github.com>
2024-04-10 02:47:38 +0000

    Atualizado versão suportada do WhatsApp Web para v2.3000.0

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -7,7 +7,7 @@ exports.DefaultOptions = {
         headless: true,
         defaultViewport: null
     },
-    webVersion: '2.2410.1',
+    webVersion: '2.3000.0',
     webVersionCache: {
         type: 'local',
     },
